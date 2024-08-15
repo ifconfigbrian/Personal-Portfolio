83022b4dd221add8bacf767ec2f2ba0b1687cbc7
Ifconfigbrian <njugunabriian@gmail.com>
2024-08-15 08:49:59 +0300

    worked on the projects component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8af585c804572be132b1"; }
+/******/ 		__webpack_require__.h = function() { return "79072377cbc77500aaec"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
